9aacd6c508d01a32e7ded236d9523d1519d9d63f
yashal-ali <yashalalifarooqui30@gmail.com>
2025-03-21 22:20:53 +0500

    change userverification

--- src/models/schemas/UserVerificationSchema.ts
+++ src/models/schemas/UserVerificationSchema.ts
@@ -16,4 +16,4 @@ const UserVerificationSchema = new Schema<IUserVerification>({
   resetTokenExpiry: { type: Date, default: null },
 });
 
-export const UserVerificationModel = mongoose.model<IUserVerification>("UserVerification", UserVerificationSchema);
+export const UserVerificationModel =mongoose.models.UserVerification ||  mongoose.model<IUserVerification>("UserVerification", UserVerificationSchema);
